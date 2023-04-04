197e804978a2dcff47e1bfeb0fe7e2e2ae6895b8
Delta1925 <lucatrucco05@gmail.com>
2023-04-04 18:46:58 +0200

    Improve css handling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			'sans': ['Alkatra', ...defaultTheme.fontFamily.sans],
+		},
+		borderWidth: {
+			'standard': '0.3vw'
+		},
+		extend: {
+		}
 	},
 	plugins: [],
 }
